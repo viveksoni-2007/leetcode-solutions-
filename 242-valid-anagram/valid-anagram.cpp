62a9957a66ec5b8936c088ad7aa5968863f2ba3f
viveksoni-2007 <viveksoni2248@gmail.com>
2026-08-08 19:06:48 +0530

    Time: 7 ms (18.36%) | Memory: 9.8 MB (26.19%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -7,23 +7,40 @@ public:
         // if(s==t) return true;
         // else return false ;
 
-        unordered_map<char,int> map1;
-        unordered_map<char,int> map2;
+        // unordered_map<char,int> map1;
+        // unordered_map<char,int> map2;
+        // for(int i = 0 ; i < s.size();i++){
+        //     map1[s[i]]++;
+        // }
+        // for(int i = 0 ; i < t.size();i++){
+        //     map2[t[i]]++;
+        // }
+        // for(auto x : map1){
+        //     char ch1 = x.first;
+        //     int frq1 = x.second ;
+        //     if(map2.find(ch1)!=map2.end()){
+        //         int frq2 = map2[ch1];
+        //         if(frq1!=frq2) return false ;
+        //     }
+        //     else return false ;
+        // }
+        // return true ;
+
+
+
+         unordered_map<char,int> map1;
         for(int i = 0 ; i < s.size();i++){
             map1[s[i]]++;
         }
-        for(int i = 0 ; i < t.size();i++){
-            map2[t[i]]++;
-        }
-        for(auto x : map1){
-            char ch1 = x.first;
-            int frq1 = x.second ;
-            if(map2.find(ch1)!=map2.end()){
-                int frq2 = map2[ch1];
-                if(frq1!=frq2) return false ;
+        for(int i = 0 ; i < t.length() ; i++){
+            char ch  =t[i];
+            if(map1.find(ch)!=map1.end()){
+                map1[ch]--;
+                if(map1[ch]==0) map1.erase(ch);
             }
             else return false ;
         }
+        if(map1.size()>0) return false ;
         return true ;
     }
 }; 
