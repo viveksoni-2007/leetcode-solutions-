72610cb777d38e93ca4468d038d2b170854bd68f
viveksoni-2007 <viveksoni2248@gmail.com>
2026-08-08 22:16:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (70.12%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>m;
+        for(int i = 0 ; i < arr.size(); i++){
+            // m[arr[i]] == arr[i];
+            m[arr[i]]++;
+        }
+        unordered_set<int>s;
+        for(auto x : m){
+            int freq = x.second ;
+            if(s.find(freq)!=s.end()) return false ;
+            else s.insert(freq);
+        }
+        return true ;
+
+
+    }
+};
