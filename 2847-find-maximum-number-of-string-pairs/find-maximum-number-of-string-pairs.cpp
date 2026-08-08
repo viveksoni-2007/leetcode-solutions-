eb99beb936e20b855c7847507fea1639e9aee3c0
viveksoni-2007 <viveksoni2248@gmail.com>
2026-08-08 16:52:02 +0530

    Time: 2 ms (64.14%) | Memory: 25.5 MB (69.13%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
@@ -1,20 +1,35 @@
+// class Solution {
+// public:
+//     int maximumNumberOfStringPairs(vector<string>& words) {
+//         int n = words.size();
+//         unordered_set<string> s ;
+//         for(int i = 0 ; i < words.size(); i++){
+//             s.insert(words[i]);
+//         }
+//         int c = 0 ;
+//         for(int i = 0 ; i < words.size();i++){
+//             string rev  = words[i];
+//             reverse(rev.begin(),rev.end());
+//             if(words[i]==rev) continue ;
+//             if(s.find(rev)!=s.end()){
+//                 c++;
+//                 s.erase(words[i]);
+//             }
+//         }
+//         return c;
+//     }
+// };
 class Solution {
 public:
     int maximumNumberOfStringPairs(vector<string>& words) {
         int n = words.size();
+        int c = 0 ;
         unordered_set<string> s ;
         for(int i = 0 ; i < words.size(); i++){
-            s.insert(words[i]);
-        }
-        int c = 0 ;
-        for(int i = 0 ; i < words.size();i++){
             string rev  = words[i];
             reverse(rev.begin(),rev.end());
-            if(words[i]==rev) continue ;
-            if(s.find(rev)!=s.end()){
-                c++;
-                s.erase(words[i]);
-            }
+            if(s.find(rev)!=s.end()) c++;
+            else s.insert(words[i]);
         }
         return c;
     }
