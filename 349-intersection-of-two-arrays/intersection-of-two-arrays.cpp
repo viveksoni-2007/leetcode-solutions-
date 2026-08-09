1f7a34dcf34002014f9db2f1d78268100d81d205
viveksoni-2007 <viveksoni2248@gmail.com>
2026-08-09 14:55:39 +0530

    Time: 1 ms (48.40%) | Memory: 14.5 MB (26.34%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans ;
-        unordered_set<int> s1(nums1.begin(), nums1.end());
-        unordered_set<int> s2(nums2.begin(), nums2.end());
-        unordered_map<int, int> m;
-         for(int x : s1)
-            m[x]++;
+        // vector<int>ans ;
+        // unordered_set<int> s1(nums1.begin(), nums1.end());
+        // unordered_set<int> s2(nums2.begin(), nums2.end());
+        // unordered_map<int, int> m;
+        //  for(int x : s1)
+        //     m[x]++;
 
-        for(int x : s2)
-            m[x]++;
+        // for(int x : s2)
+        //     m[x]++;
 
-        for(auto x : m) {
-            if(x.second == 2)
-                ans.push_back(x.first);
+        // for(auto x : m) {
+        //     if(x.second == 2)
+        //         ans.push_back(x.first);
+        // }
+
+        // return ans;
+         unordered_set<int> s1, s2;
+        vector<int> ans;
+
+        for(int x : nums1)
+            s1.insert(x);
+
+        for(int x : nums2)
+            s2.insert(x);
+
+        for(int x : s1) {
+            if(s2.count(x))
+                ans.push_back(x);
         }
 
         return ans;
