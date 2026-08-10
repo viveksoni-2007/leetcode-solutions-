1c2eb5f714c4e260becadf334c5f24a4638c54aa
viveksoni-2007 <viveksoni2248@gmail.com>
2026-08-10 22:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (14.96%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>m;
+        for(char x:s ){
+            m[x]++;
+        }
+        for(char x:t ){
+            m[x]--;
+        }
+        for(auto x : m){
+            if(x.second!=0){
+                return x.first;
+            }
+        }
+        return ' ' ;
+    }
+};
